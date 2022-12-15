43e8fa9c1e403b034128aae3f232200724dad1e7
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2022-12-15 11:13:02 +0700

    Progress_3

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,8 @@
 package entity;
 
 import java.awt.Color;
+
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -21,6 +23,7 @@ public class Player extends Entity {
 	public final int screenX;
 	public final int screenY;
 	public int hasKey = 0;
+	public int hasPet = 0;
 	
 	public Player(GamePanel gp, KeyHandler KeyH) {
 		
@@ -47,20 +50,25 @@ public class Player extends Entity {
 		worldX = gp.tileSize * 23;
 		worldY = gp.tileSize * 21;
 		speed = 4;
-		direction = "down";
+	   	direction = "down";
+	   	
+	   	//Player status nyawa
+	   	maxLife = 6;
+	   	life = maxLife;
 		
 	}
 	public void getPlayerImage() {
 		try {
 			
-			up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-			up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-			down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-			down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-			left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-			left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
-			right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-			right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+			up1 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/belakang_kanan.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/belakang_kiri.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/depan_kanan.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/depan_kiri.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/langkah_pendek_kiri.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/langkah_panjang_kiri.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/langkah pendek kanan.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/nwplayer/langkah_panjang_kanan.png"));
+			logo = ImageIO.read(getClass().getResourceAsStream("/asset1/logobig2.png"));
 			
 		}catch(IOException e) {
 			e.printStackTrace();
@@ -129,7 +137,7 @@ public class Player extends Entity {
 				gp.playSE(1);
 				hasKey++;
 				gp.obj[i] = null;
-				gp.ui.showMessage("You got a key");
+				gp.ui.showMessage("Key Found!");
 				
 				break;
 			case "Door" :
@@ -137,23 +145,28 @@ public class Player extends Entity {
 					gp.playSE(3);
 					gp.obj[i] = null;
 					hasKey--;
-					gp.ui.showMessage("You opened the door!");
+					gp.ui.showMessage("Berhasil buka pintu");
 				}
 				else {
-					gp.ui.showMessage("You need a key!");
+					gp.ui.showMessage("Cari dulu kuncinya!");
 				}
-				System.out.println("Key:"+hasKey);
 				break;
 			case "Boots" :
 				gp.playSE(2);
 				speed += 4;
 				gp.obj[i] = null;
-				gp.ui.showMessage("Speed up!");
+				gp.ui.showMessage("Speed Up!");
 				break;
-			case "Chest":
+			case "Chest" :
 				gp.ui.gameFinished = true;
 				gp.stopMusic();
 				gp.playSE(4);
+				break;
+			case "Pet" :
+				gp.playSE(2);
+				hasPet++;
+				gp.obj[i] = null;
+				gp.ui.showMessage("yeay you found the pet!");
 				break;
 			}
 		}
--- src/pet/com/CollisionChecker.java
+++ src/pet/com/CollisionChecker.java
@@ -79,6 +79,7 @@ public class CollisionChecker {
 				case "up" :
 					entity.solidArea.y -= entity.speed;
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+
 						if(gp.obj[i].collision == true) {
 							entity.collisionOn = true;
 						}
@@ -90,6 +91,7 @@ public class CollisionChecker {
 				case "down" :
 					entity.solidArea.y += entity.speed;
 					if(entity.solidArea.intersects(gp.obj[i].solidArea)) {
+
 						if(gp.obj[i].collision == true) {
 							entity.collisionOn = true;
 						}
